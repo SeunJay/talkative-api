ab3e46ade3b6ff806f7d101130f71b60d21cdc6c
Oluwaseun Jayeoba <seunjay92@gmail.com>
2022-05-05 20:45:03 +0100

    connect to mongodb

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 const app = express();
 
 dotenv.config();
 
+connectDB();
+
 app.get('/', (_, res) => {
   res.send('API is running');
 });
 
-
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`server started on ${port}`);
+  console.log(`server started on ${port}`.yellow.bold);
 });
